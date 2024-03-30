ec83a1f8edccfc68cd45cd7f8fc007286a83b427
Hernan Solorzano Ramirez <87883576+Hsolorzano1987@users.noreply.github.com>
2024-03-30 18:03:06 -0500

    first puch

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Iste laborum repellendus magni impedit ut quisquam temporibus,
+             ratione atque dicta mollitia, deserunt sapiente ducimus distinctio 
+             officia unde omnis quidem pariatur, totam vitae reiciendis laboriosam. 
+             Eligendi maiores totam quod nobis iste adipisci voluptates veritatis
+             architecto ut impedit. Quaerat quod repellat at cum aspernatur dolorum
+             saepe impedit quisquam omnis exercitationem reiciendis explicabo 
+               distinctio facere perferendis suscipit beatae cumque, dignissimos 
+               ducimus assumenda vitae eveniet voluptatem expedita itaque et! 
+               Ratione culpa dicta tempore itaque beatae illum laboriosam nulla, 
+               eius fuga optio id deleniti harum sint dolores fugiat a, velit rerum
+             adipisci debitis.
+             Exercitationem, consequatur quidem?</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,16 @@
+function ServicePage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatem voluptate non sint culpa deleniti ad, natus asperiores 
+            commodi ducimus harum architecto quod ut facilis officia perferendis
+             illum praesentium numquam eligendi, molestiae doloremque blanditiis
+              consequuntur tempora in. Est incidunt vero magnam ipsa saepe id, 
+              adipisci eveniet neque labore. 
+            Deserunt, necessitatibus doloremque!</p>
+    </div>
+  )
+}
+
+export default ServicePage
